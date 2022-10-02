05d6e425fe9c4be4f5c0f54ff14b6a2e76ac2c98
João Pedro de Oliveira <joaopedro_o@outlook.com>
2022-10-02 19:39:40 -0300

    voltando para alguma versão anterior

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-console.log("Fiz esse commit em cloud para verificar o git pull")
-// Enviando para o GitHub
-console.log("Estou enviando mais uma modificação para verifical git push")
