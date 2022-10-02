2c439372f1a12f29a5c6dde04915aa395e1a6cf5
João Pedro de Oliveira <91745957+oJoaoPeOliveira@users.noreply.github.com>
2022-10-02 18:55:20 -0300

    adicionando console para testar git pull

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+console.log("Fiz esse commit em cloud para verificar o git pull")
