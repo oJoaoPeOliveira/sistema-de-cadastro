70122287a83bb820faf04d32269b39cbcca0edf4
João Pedro de Oliveira <joaopedro_o@outlook.com>
2022-10-02 19:04:02 -0300

    testando git push

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 console.log("Fiz esse commit em cloud para verificar o git pull")
+// Enviando para o GitHub
+console.log("Estou enviando mais uma modificação para verifical git push")
